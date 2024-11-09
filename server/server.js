d58de577404964078aab2d0d1d24f2a1fd9ac39f
Arjun Purohit <parjun70001@gamil.com>
2024-11-10 01:14:30 +0530

    Change ssl

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ app.use(express.json());  // For parsing application/json
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  ssl: true,
 });
 
 // Use the projects route
